8119b8523e7fb6331649683d8f2629bfaa0888c7
Mr <jpihlk@yahoo.com>
2025-09-17 21:04:36 -0400

    added info on project

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
-import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import {
+  McpServer,
+  ResourceTemplate,
+} from "@modelcontextprotocol/sdk/server/mcp.js";
+// meaning we are going to communicate through the terminal only
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 
 // Create an MCP server
 const server = new McpServer({
   name: "demo-server",
-  version: "1.0.0"
+  version: "1.0.0",
 });
 
 // Add an addition tool
-server.registerTool("add",
+server.registerTool(
+  "add",
   {
     title: "Addition Tool",
     description: "Add two numbers",
-    inputSchema: { a: z.number(), b: z.number() }
+    inputSchema: { a: z.number(), b: z.number() },
   },
   async ({ a, b }) => ({
-    content: [{ type: "text", text: String(a + b) }]
+    content: [{ type: "text", text: String(`${a} + ${b} Hello world!`) }],
   })
 );
+
 
 // Add a dynamic greeting resource
 server.registerResource(
   "greeting",
   new ResourceTemplate("greeting://{name}", { list: undefined }),
-  { 
-    title: "Greeting Resource",      // Display name for UI
-    description: "Dynamic greeting generator"
+  {
+    title: "Greeting Resource", // Display name for UI
+    description: "Dynamic greeting generator",
   },
   async (uri, { name }) => ({
-    contents: [{
-      uri: uri.href,
-      text: `Hello, ${name}!`
-    }]
+    contents: [
+      {
+        uri: uri.href,
+        text: `Hello, ${name}!`,
+      },
+    ],
   })
 );
 
